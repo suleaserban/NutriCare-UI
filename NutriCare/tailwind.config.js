c3c030b2c68173356e93242c30d1bd3994556e31
Sulea Serban <serban.sulea@ibm.com>
2024-02-20 19:02:53 +0200

    added changes

--- NutriCare/tailwind.config.js
+++ NutriCare/tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {fontFamily: {
-      montserrat: ['Montserrat', 'sans-serif'],
-      'flama': ['Flama', 'sans-serif']
-    },},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        flama: ["Flama", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
